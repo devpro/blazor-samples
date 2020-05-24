27f4267597c065b517c6713defbf9650575b6f86
Bertrand THOMAS <bertrand@devpro.fr>
2020-05-24 10:26:23 +0200

    Feature/Migrate WebAssembly Blazor lib to 3.2.0 (#5)
    * Migrate WebAssembly project to 3.2.0-rc1.20223.4
    
    * Update web assembly blazor app to official 3.2.0

--- src/WebAssemblyBlazorApp/Program.cs
+++ src/WebAssemblyBlazorApp/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -11,7 +13,7 @@ namespace Devpro.BlazorSamples.WebAssemblyBlazorApp
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
 
-            builder.Services.AddBaseAddressHttpClient();
+            builder.Services.AddSingleton(new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             await builder.Build().RunAsync();
         }
